b8516ae727470a67ecc25304ef247eed01911446
nandusri <kasu.nandu@gmail.com>
2020-07-31 23:37:16 +0530

    signup and login backend functionality implemented

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import SignUpForm
+
+from django.http import HttpResponse
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            # return redirect('home')
+            return HttpResponse('<h3>Account created successfully</h3>')
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
